eb4d9fc54f71a37db7a738f164d258523082a2b0
Anderson Filho <filhoando@gmail.com>
2024-02-10 18:22:16 -0300

    Tesseract e adicionando boleto

--- testes.py
+++ testes.py
@@ -1,7 +1,9 @@
-import pyautogui as py
+import cv2
+import pytesseract
 
-py.sleep(4),py.press('PageUP'),py.sleep(1)
-imagem_pagar = py.locateCenterOnScreen('C:\\Users\\filho\\PycharmProjects\\Detran_loc\\pagar.png',confidence=0.7)
-py.click(imagem_pagar.x,imagem_pagar.y)
+img = cv2.imread("boleto.pdf")
+pytesseract.pytesseract.tesseract_cmd = "C:/Users/filho/tesseract.exe"
+res = pytesseract.image_to_string(img)
+print(res)
 
-
+#lembrando de instalar o pytesseract
